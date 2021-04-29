ae55e563ab9b551f23cc98fc1b2a772f510e3c88
SophieClarkKC <sophie.clark@kinandcarta.com>
2021-04-29 10:56:20 +0100

    initial commit

---
+++ TideApp/TideApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TideApp
+//
+//  Created by Sophie Clark on 29/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TideApp/TideApp/TideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TideAppApp.swift
+//  TideApp
+//
+//  Created by Sophie Clark on 29/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct TideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
