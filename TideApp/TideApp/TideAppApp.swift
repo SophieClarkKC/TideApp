5171c39f4c51850ac67173fd98032625d1917c5d
Dan Smtih <dan.smith@kinandcarta.com>
2021-05-05 09:56:21 +0100

    Indentation

--- TideApp/TideApp/TideAppApp.swift
+++ TideApp/TideApp/TideAppApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct TideAppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
