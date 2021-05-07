c306c511e4f1e66fc4f5c595b4daaf2210ba33d9
Marco Guerrieri <marco.guerrieri@theappbusiness.com>
2021-05-07 17:45:40 +0100

    Added Mock server start

--- TideApp/TideApp/App/AppDelegate.swift
+++ TideApp/TideApp/App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MockServer
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
