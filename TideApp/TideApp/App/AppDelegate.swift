599baff37ac7e7abe060f24a98adcf2715fb7014
Marco Guerrieri <marco.guerrieri@theappbusiness.com>
2021-05-07 15:56:43 +0100

    Added Mock server start

--- TideApp/TideApp/App/AppDelegate.swift
+++ TideApp/TideApp/App/AppDelegate.swift
@@ -10,6 +10,21 @@ import UIKit
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     print("Application started with\nBase URL: \(AppConfig.baseURL)\nEnvironment: \(AppConfig.isProdEnv ? "Production" : "Mock")")
+
+    #if DEBUG
+    startMockServerIfNeeded()
+    #endif
+
     return true
   }
+}
+
+extension AppDelegate {
+  #if DEBUG
+  private func startMockServerIfNeeded() {
+    if AppConfig.isTestEnv {
+      MockServer().start()
+    }
+  }
+  #endif
 }
