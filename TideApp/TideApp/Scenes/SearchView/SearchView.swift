49922426e690425806bda4ed9fa6ca24e5b38179
John Sanderson <john.sanderson@theappbusiness.com>
2021-05-18 11:34:15 +0100

    Perform search using MKLocalSearch and present results in list

--- TideApp/TideApp/Scenes/SearchView/SearchView.swift
+++ TideApp/TideApp/Scenes/SearchView/SearchView.swift
@@ -19,15 +19,66 @@ struct SearchView: View {
 
   var body: some View {
     NavigationView {
-      List {
-        ForEach(viewModel.searchResults, id: \.self) { Text($0) }
-      }
-      .navigationTitle("Search")
-      .overlay(
-        ViewControllerResolver { viewController in
-          viewController.navigationItem.searchController = searchControllerProvider.searchController
+      makeView(for: viewModel.state)
+        .navigationTitle("Search")
+        .overlay(
+          ViewControllerResolver { viewController in
+            viewController.navigationItem.searchController = searchControllerProvider.searchController
+          }.frame(width: 0, height: 0)
+        )
+        .onAppear {
+          viewModel.start()
         }
-      )
+    }
+  }
+
+  private func makeView(for state: SearchViewModel.State) -> AnyView {
+    switch viewModel.state {
+    case .idle:
+      return AnyView(IdleView())
+    case .error, .noResults:
+      return AnyView(NoResultsView())
+    case .success(let results):
+      return AnyView(ResultsView(results: results))
+    }
+  }
+}
+
+// MARK: - View States -
+
+private struct IdleView: View {
+
+  var body: some View {
+    VStack(alignment: .center, spacing: PaddingValues.medium) {
+      SubtitleLabel(text: "Search for a location", alignment: .center)
+        .multilineTextAlignment(.center)
+      BodyLabel(text: "Tide information will be shown for the nearest marine weather station to your chosen location.", alignment: .center)
+      Spacer()
+    }
+    .padding()
+  }
+}
+
+private struct NoResultsView: View {
+
+  var body: some View {
+    SubtitleLabel(text: "Sorry, no results match this search!", alignment: .center)
+      .multilineTextAlignment(.center)
+      .padding()
+  }
+}
+
+private struct ResultsView: View {
+
+  let results: [String]
+
+  var body: some View {
+    List {
+      ForEach(results, id: \.self) { result in
+        Button(action: {}) {
+          Text(result)
+        }
+      }
     }
   }
 }
