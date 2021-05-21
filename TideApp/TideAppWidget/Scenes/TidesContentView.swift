0404aba430e8110dd65506ff186c3a71e417f72a
Marco <marco.guerrieri@kinandcarta.com>
2021-05-21 13:05:06 +0100

    Fixes

--- TideApp/TideAppWidget/Scenes/TidesContentView.swift
+++ TideApp/TideAppWidget/Scenes/TidesContentView.swift
@@ -36,18 +36,22 @@ private struct SmallSizeView: View {
   var viewModel: TidesContentViewModel
 
   var body: some View {
-    VStack(alignment: .leading, spacing: 0) {
-      Spacer()
-      SubtitleLabel(text: viewModel.place)
-      if let tideStatus = viewModel.tideStatus {
-        Spacer(minLength: 1)
-        BodyLabel(text: tideStatus)
+    HStack {
+      Spacer(minLength: 4)
+      VStack(alignment: .leading, spacing: 0) {
+        Spacer()
+        SubtitleLabel(text: viewModel.place)
+        if let tideStatus = viewModel.tideStatus {
+          Spacer(minLength: 1)
+          BodyLabel(text: tideStatus)
+        }
+        if let waterTemperature = viewModel.waterTemperature {
+          Spacer(minLength: 1)
+          BodyLabel(text: waterTemperature)
+        }
+        Spacer(minLength: 2)
       }
-      if let waterTemperature = viewModel.waterTemperature {
-        Spacer(minLength: 1)
-        BodyLabel(text: waterTemperature)
-      }
-      Spacer(minLength: 2)
+      Spacer(minLength: 4)
     }
   }
 }
@@ -87,27 +91,18 @@ private struct LargeSizeView: View {
 
   var body: some View {
     VStack(alignment: .center, spacing: 2) {
-      Spacer(minLength: 2)
-      SubtitleLabel(text: viewModel.place)
-      if viewModel.hasPrincipalInfos() {
-        Spacer(minLength: 2)
+      MediumSizeView(viewModel: viewModel)
+      Divider()
+      HStack {
+        Spacer(minLength: 12)
+        TideChartView(animate: true,
+                      tideData: viewModel.tidesTimes ?? [],
+                      tideHeight: viewModel.tideHeight)
+          .frame(maxWidth: .infinity,
+                 minHeight: 140,
+                 alignment: .center)
+        Spacer(minLength: 12)
       }
-
-      if let tideStatus = viewModel.tideStatus {
-        BodyLabel(text: tideStatus)
-      }
-      if let waterTemperature = viewModel.waterTemperature {
-        BodyLabel(text: waterTemperature)
-      }
-
-      if let tidesTimes = viewModel.tidesTimes {
-        Spacer(minLength: 2)
-        Divider()
-        Spacer(minLength: 2)
-        TidesTimesView(tidesTimes: tidesTimes)
-        Spacer(minLength: 2)
-      }
-      Spacer(minLength: 2)
     }
   }
 }
